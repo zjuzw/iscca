8b8ce9f89ff054301f2d1494dc4abf90a34acaa4
zjuzw <zju_zw@zju.edu.cn>
2016-10-30 20:37:05 +0800

    Correction

--- iscca/plot_iscca_data.m
+++ iscca/plot_iscca_data.m
@@ -1,29 +1,25 @@
 clear;clc;close all;
 load('.\ISC\isc_data');
-f=1:1.8e3;f=f-1;f=f/1.8e3;f=f*200;
-%%
+% The MEG signals were decimated to 200 Hz
+f=1:2e3;f=f-1;f=f/2e3;f=f*200;
+t = 1:2000;t=t/200;
+% Response waveform
 figure(1);
 for cp=1:6
-    subplot(6,1,cp);
-    plot(isc_data(:,:,cp),'color',[1 1 1]*.8);hold on;
-    plot(mean(isc_data(:,:,cp),2),'k','linewidth',2);
+    subplot(3,2,cp);
+    plot(t,isc_data(:,:,cp),'color',[1 1 1]*.8);hold on;
+    plot(t,mean(isc_data(:,:,cp),2),'k','linewidth',2);
+    xlabel('time');
+    ylabel('power');
 end
-%%
+% Response spectrum
 figure(2);
 for cp=1:6
-    subplot(6,1,cp);
-    fft_data = abs(fft(isc_data(1:1.8e3,:,cp)));
+    subplot(3,2,cp);
+    fft_data = abs(fft(isc_data(:,:,cp)));
     plot(f,fft_data,'color',[1 1 1]*.8);hold on;
     plot(f,mean(fft_data,2),'k','linewidth',2);
     xlim([.5 4.5])
-end
-%%
-figure(3);
-for cp=1:6
-    subplot(6,1,cp);
-    fft_data = abs(fft(isc_data(1:1.8e3,:,cp)));
-    fft_timemean_data = abs(fft(mean(isc_data(1:1.8e3,:,cp),2)));
-    plot(f,fft_data,'color',[1 1 1]*.8);hold on;
-    plot(f,mean(fft_data,2),'k','linewidth',2);
-    xlim([.5 4.5])
+    xlabel('Frequency');
+    ylabel('Amplitude (a.u.)');  
 end
